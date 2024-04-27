dee0ee5feb5ec5328d4e5998b7c44f61c6d1b61a
datsiii <dasha.tsivina@gmail.com>
2024-04-27 12:42:56 +0300

    init splash + bottom menu

---
+++ app/src/main/java/com/example/berryapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.berryapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHost.navController
+        bottomNavigation.setupWithNavController(navController)
+    }
+}
